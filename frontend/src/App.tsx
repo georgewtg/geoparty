d62d77ac81347ed687e61005c32a3fbc16c6afc2
George Wiliam Thomas Gonata <georgewiliam6039@gmail.com>
2026-09-05 20:40:06 +0700

    Add back default routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route} from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import PublicOnlyRoute from './components/PublicOnlyRoute';
 import ProtectedRoute from './components/ProtectedRoute';
 import Menu from './components/Menu';
@@ -13,6 +13,8 @@ import './App.css';
 const App: React.FC = () => {
   return (
     <Routes>
+      <Route path='/' element={<Navigate to="/login" replace />} />
+
       {/* accessible only if not logged in */}
       <Route element={<PublicOnlyRoute />}>
         <Route path='/login' element={<Login />} />
